e03a57d5e9f18ff72aa258d6ecbac9f1934f8f83
Zereges <zereges@gmail.com>
2015-02-18 17:26:44 +0100

    NumberedRect: m_number initialization, correct BLOCK_COLORS index.

--- src/Definitions/NumberedRect.hpp
+++ src/Definitions/NumberedRect.hpp
@@ -11,7 +11,7 @@ class NumberedRect : public Rect
 {
     public:
         NumberedRect(const SDL_Point& point, unsigned int number, std::size_t w = Definitions::BLOCK_SIZE_X, std::size_t h = Definitions::BLOCK_SIZE_Y) :
-            Rect(point, Definitions::BLOCK_COLORS[logb(number)], w, h) { }
+            Rect(point, Definitions::BLOCK_COLORS[number], w, h) { m_number = number; }
         virtual ~NumberedRect() { }
         unsigned int get_number() { return m_number; }
         virtual void draw() const { Rect::draw(); }
